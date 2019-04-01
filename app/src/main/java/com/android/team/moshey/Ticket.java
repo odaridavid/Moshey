027c49e39190d301b4f74ae87fcb6722f739e9a9
Davidodari <davidkibzodari@gmail.com>
2019-04-01 22:12:36 +0300

    Initial commit

---
+++ app/src/main/java/com/android/team/moshey/Ticket.java
@@ -0,0 +1,12 @@
+package com.android.team.moshey;
+
+/**
+ * Created By blackcoder
+ * On 01/04/19
+ **/
+public final class Ticket {
+    private String ticketId;
+    private String destinationPoint;
+    private String departurePoint;
+    private String date;
+}
