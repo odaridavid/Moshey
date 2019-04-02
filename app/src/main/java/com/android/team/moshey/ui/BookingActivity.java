d6b24c95eb95da8632f9d0b70fdf8487b707b6c7
Davidodari <davidkibzodari@gmail.com>
2019-04-02 15:19:43 +0300

    [Feature #165036044] Enhance app skeletal structure

--- app/src/main/java/com/android/team/moshey/ui/BookingActivity.java
+++ app/src/main/java/com/android/team/moshey/ui/BookingActivity.java
@@ -1,10 +1,12 @@
-package com.android.team.moshey;
+package com.android.team.moshey.ui;
 
 import android.os.Bundle;
 
+import com.android.team.moshey.R;
 import com.android.team.moshey.databinding.ActivityBookingBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 
 public class BookingActivity extends AppCompatActivity {
@@ -13,5 +15,12 @@ public class BookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityBookingBinding vBookingBinding = DataBindingUtil.setContentView(this, R.layout.activity_booking);
+        Toolbar vToolbar = vBookingBinding.bookingAppBar;
+        vToolbar.setTitle(getString(R.string.book_title));
+        setSupportActionBar(vToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        }
     }
 }
